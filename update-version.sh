b594f1864d0139c6a57ef81faada6a9f9bbe4a65
Emmanuele Somma <emmanuele@exedre.org>
2024-11-26 18:05:50 +0100

    version also in comments

--- update-version.sh
+++ update-version.sh
@@ -8,7 +8,7 @@ VERSION=$(git describe --tags --abbrev=0)
 
 # Aggiorna il file con la nuova versione
 if [[ -n "$VERSION" ]]; then
-  sed -i.bak -E "s/(defconst strkm-version \")[^\"]*(\")/\1$VERSION\2/" "$FILE"
+  sed -i.bak -E -e "s|(;; Version: ).*|\1$VERSION|" -e "s|(defconst strkm-version \")[^\"]*(\")|\1$VERSION\2|" "$FILE"
   echo "Updated version in $FILE to $VERSION"
 else
   echo "No Git tag found. Version not updated."
